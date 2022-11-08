2bf45e12df3682945563f8e99368ff981291f794
Dennis Ping <idennisping@gmail.com>
2022-11-07 23:40:41 -0500

    Finish a7 lab

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import HelloController from "./controllers/hello-controller";
 import UserController from "./controllers/users/users-controller";
 
 const app = express();
+app.use(express.json());
 HelloController(app);
 UserController(app);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
 });
--- src/controllers/users/users-controller.ts
+++ src/controllers/users/users-controller.ts
@@ -12,10 +12,46 @@ let users: user[] = people;
 
 const UserController = (app: Express) => {
   app.get("/api/users", findUsers);
+  app.get("/api/users/:uid", findUserById);
+  app.post("/api/users", createUser);
+  app.delete("/api/users/:uid", deleteUser);
+  app.put("/api/users/:uid", updateUser);
 };
 
 const findUsers = (req: Request, res: Response) => {
+  const type = req.query.type;
+  if (type) {
+    const usersOfType = users.filter((u) => u.type === type);
+    res.json(usersOfType);
+    return;
+  }
   res.json(users);
+};
+
+const findUserById = (req: Request, res: Response) => {
+  const userId = req.params.uid;
+  const user = users.filter((u) => u._id === userId);
+  res.json(user);
+};
+
+const createUser = (req: Request, res: Response) => {
+  const newUser: user = req.body;
+  newUser._id = new Date().getTime().toString();
+  users.push(newUser);
+  res.json(newUser);
+};
+
+const deleteUser = (req: Request, res: Response) => {
+  const userId = req.params.uid;
+  users = users.filter((u) => u._id !== userId);
+  res.sendStatus(200);
+};
+
+const updateUser = (req: Request, res: Response) => {
+  const userId = req.params.uid;
+  const updates = req.body;
+  users = users.map((u) => (u._id === userId ? { ...u, ...updates } : u));
+  res.sendStatus(200);
 };
 
 export default UserController;
