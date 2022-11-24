6c51ff20d36304bf7638e45f0ec0e8f63506d705
Dennis Ping <idennisping@gmail.com>
2022-11-24 14:30:18 -0500

    Trigger a deploy

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import TuitsController from "./controllers/tuits/tuits-controller";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/tuiter";
 mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
